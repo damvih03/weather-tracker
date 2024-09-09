a220ee70ab39fe6f551dfa3f202b9846dd41cff4
damvih <damvih03.official@gmail.com>
2024-09-10 09:01:48 +1000

    feat: add Dao

---
+++ src/main/java/com/damvih/dao/Dao.java
@@ -0,0 +1,56 @@
+package com.damvih.dao;
+
+import com.damvih.exceptions.DatabaseOperationException;
+import com.damvih.utils.PersistenceUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceException;
+import org.hibernate.exception.ConstraintViolationException;
+
+abstract public class Dao<T> {
+
+    public void delete(T entity) {
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = PersistenceUtil.getInstance().createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.remove(entity);
+            transaction.commit();
+        } catch (PersistenceException exception) {
+            rollbackTransaction(transaction);
+            throw new DatabaseOperationException();
+        }
+    }
+
+    public T save(T entity) {
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = PersistenceUtil.getInstance().createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(entity);
+            transaction.commit();
+            return entity;
+        } catch (PersistenceException exception) {
+            rollbackTransaction(transaction);
+            if (isConstraintViolationException(exception)) {
+                handleConstraintViolationException();
+            }
+            throw new DatabaseOperationException();
+        }
+    }
+
+    protected void handleConstraintViolationException() {
+
+    }
+
+    protected void rollbackTransaction(EntityTransaction transaction) {
+        if (transaction != null && transaction.isActive()) {
+            transaction.rollback();
+        }
+    }
+
+    private boolean isConstraintViolationException(PersistenceException exception) {
+        return exception instanceof ConstraintViolationException;
+    }
+
+}
