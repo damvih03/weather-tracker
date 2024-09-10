ca5b7c0fb66cf64d6771ab056c63b6425dfc824a
damvih <damvih03.official@gmail.com>
2024-09-11 09:44:10 +1000

    feat: add UserRegistrationDto

---
+++ src/main/java/com/damvih/dto/UserRegistrationDto.java
@@ -0,0 +1,16 @@
+package com.damvih.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegistrationDto {
+
+    private String login;
+    private String password;
+    private String confirmedPassword;
+
+}
