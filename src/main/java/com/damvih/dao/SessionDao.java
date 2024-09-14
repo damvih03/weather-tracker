be6d11d8f282851ae92421a2c5db3c1312963e2b
damvih <damvih03.official@gmail.com>
2024-09-14 13:41:50 +1000

    feat: add deleteByUser method refactor: delete unused imports

--- src/main/java/com/damvih/dao/SessionDao.java
+++ src/main/java/com/damvih/dao/SessionDao.java
@@ -1,13 +1,14 @@
 package com.damvih.dao;
 
 import com.damvih.entities.Session;
+import com.damvih.entities.User;
 import com.damvih.exceptions.DatabaseOperationException;
 import com.damvih.utils.PersistenceUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceException;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,6 +24,24 @@ public class SessionDao extends Dao<Session> {
 
             transaction.commit();
             return Optional.ofNullable(session);
+        } catch (PersistenceException exception) {
+            rollbackTransaction(transaction);
+            throw new DatabaseOperationException();
+        }
+    }
+
+    public void deleteByUser(User user) {
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = PersistenceUtil.getInstance().createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            entityManager
+                    .createQuery("delete from Session s where user=:user")
+                    .setParameter("user", user)
+                    .executeUpdate();
+
+            transaction.commit();
         } catch (PersistenceException exception) {
             rollbackTransaction(transaction);
             throw new DatabaseOperationException();
