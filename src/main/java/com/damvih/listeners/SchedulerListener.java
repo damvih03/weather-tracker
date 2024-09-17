d7947e27a461d336a8779a55508fc912af1e0f08
damvih <damvih03.official@gmail.com>
2024-09-17 21:48:49 +1000

    feat: add SchedulerListener

---
+++ src/main/java/com/damvih/listeners/SchedulerListener.java
@@ -0,0 +1,24 @@
+package com.damvih.listeners;
+
+import com.damvih.utils.SchedulerUtil;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class SchedulerListener implements ServletContextListener {
+
+    private SchedulerUtil schedulerUtil;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        schedulerUtil = new SchedulerUtil();
+        schedulerUtil.start();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        schedulerUtil.stop();
+    }
+
+}
