9ea9dea96fbaced3ef9d33ee0d9e4345da96caba
damvih <damvih03.official@gmail.com>
2024-09-20 16:33:43 +1000

    fix: use Getter and Getter annotations instead Data

--- src/main/java/com/damvih/dto/UserRegistrationDto.java
+++ src/main/java/com/damvih/dto/UserRegistrationDto.java
@@ -1,10 +1,12 @@
 package com.damvih.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationDto {
