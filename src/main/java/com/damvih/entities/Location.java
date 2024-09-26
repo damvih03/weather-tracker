30b645df156225db383927d900af9971e35f2ea3
damvih <damvih03.official@gmail.com>
2024-09-26 19:02:32 +1000

    feat: add Location entity

---
+++ src/main/java/com/damvih/entities/Location.java
@@ -0,0 +1,27 @@
+package com.damvih.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private BigDecimal longitude;
+
+    private BigDecimal latitude;
+
+}
