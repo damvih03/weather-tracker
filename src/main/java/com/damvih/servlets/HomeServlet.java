e9b0970d46268f572b5f4bfd60f00edfed1fc44b
damvih <damvih03.official@gmail.com>
2024-09-27 20:33:08 +1000

    feat: use UserLocationDao in HomeServlet

--- src/main/java/com/damvih/servlets/HomeServlet.java
+++ src/main/java/com/damvih/servlets/HomeServlet.java
@@ -1,5 +1,9 @@
 package com.damvih.servlets;
 
+import com.damvih.dao.UserLocationDao;
+import com.damvih.entities.Location;
+import com.damvih.entities.Session;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +11,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/home", ""})
 public class HomeServlet extends HttpServlet {
 
+    private UserLocationDao userLocationDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        userLocationDao = (UserLocationDao) config
+                .getServletContext()
+                .getAttribute("UserLocationDao");
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Session session = ((Session) request.getServletContext().getAttribute("session"));
+        List<Location> locations = userLocationDao.findLocationsByUser(session.getUser());
+        request.setAttribute("userLocations", locations);
         request.getRequestDispatcher("/WEB-INF/index.html").forward(request, response);
     }
 
