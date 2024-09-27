2368881838e1f7ac2a7820ec6ebf4d4085470b28
damvih <damvih03.official@gmail.com>
2024-09-27 19:57:03 +1000

    feat: add UserLocationService

---
+++ src/main/java/com/damvih/services/UserLocationService.java
@@ -0,0 +1,27 @@
+package com.damvih.services;
+
+import com.damvih.dao.UserLocationDao;
+import com.damvih.entities.Location;
+import com.damvih.entities.User;
+import com.damvih.entities.UserLocation;
+import com.damvih.entities.UserLocationCompositeKey;
+
+public class UserLocationService {
+
+    private final UserLocationDao userLocationDao;
+
+    public UserLocationService(UserLocationDao userLocationDao) {
+        this.userLocationDao = userLocationDao;
+    }
+
+    public UserLocation save(User user, Location location) {
+        UserLocationCompositeKey userLocationCompositeKey = new UserLocationCompositeKey(user, location);
+        return userLocationDao.save(new UserLocation(userLocationCompositeKey));
+    }
+
+    public void delete(User user, Location location) {
+        UserLocationCompositeKey userLocationCompositeKey = new UserLocationCompositeKey(user, location);
+        userLocationDao.delete(new UserLocation(userLocationCompositeKey));
+    }
+
+}
