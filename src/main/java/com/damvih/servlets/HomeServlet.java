76ec0aa4b80a52d3c3a530ce6210e4f12c2ee7aa
damvih <damvih03.official@gmail.com>
2024-09-27 22:35:49 +1000

    feat: add getting weathers for user locations

--- src/main/java/com/damvih/servlets/HomeServlet.java
+++ src/main/java/com/damvih/servlets/HomeServlet.java
@@ -1,8 +1,13 @@
 package com.damvih.servlets;
 
 import com.damvih.dao.UserLocationDao;
+import com.damvih.dto.LocationRequestDto;
+import com.damvih.dto.api.geocoding.LocationApiDto;
+import com.damvih.dto.api.weather.WeatherApiResponseDto;
 import com.damvih.entities.Location;
 import com.damvih.entities.Session;
+import com.damvih.services.WeatherApiService;
+import com.damvih.utils.MapperUtil;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,15 +16,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/home", ""})
 public class HomeServlet extends HttpServlet {
 
+    private WeatherApiService weatherApiService;
     private UserLocationDao userLocationDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        weatherApiService = (WeatherApiService) config
+                .getServletContext()
+                .getAttribute("WeatherApiService");
+
         userLocationDao = (UserLocationDao) config
                 .getServletContext()
                 .getAttribute("UserLocationDao");
@@ -28,8 +41,16 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Session session = ((Session) request.getServletContext().getAttribute("session"));
+
         List<Location> locations = userLocationDao.findLocationsByUser(session.getUser());
-        request.setAttribute("userLocations", locations);
+        Map<Location, WeatherApiResponseDto> locationsWeathers = new HashMap<>();
+        for (Location location : locations) {
+            BigDecimal longitude = location.getLongitude();
+            BigDecimal latitude = location.getLatitude();
+            locationsWeathers.put(location, weatherApiService.getWeatherByCoordinates(longitude, latitude));
+        }
+
+        request.setAttribute("locationsWeathers", locationsWeathers);
         request.getRequestDispatcher("/WEB-INF/index.html").forward(request, response);
     }
 
