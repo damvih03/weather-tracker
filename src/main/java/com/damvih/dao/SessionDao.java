b1566c789ae11f58e13b36038674d4af6e41517c
damvih <damvih03.official@gmail.com>
2024-09-28 13:17:17 +1000

    refactor: remove deleteByUser method in SessionDao

--- src/main/java/com/damvih/dao/SessionDao.java
+++ src/main/java/com/damvih/dao/SessionDao.java
@@ -1,7 +1,6 @@
 package com.damvih.dao;
 
 import com.damvih.entities.Session;
-import com.damvih.entities.User;
 import com.damvih.exceptions.DatabaseOperationException;
 import com.damvih.utils.PersistenceUtil;
 import jakarta.persistence.EntityManager;
@@ -9,7 +8,6 @@ import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceException;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,24 +23,6 @@ public class SessionDao extends Dao<Session> {
 
             transaction.commit();
             return Optional.ofNullable(session);
-        } catch (PersistenceException exception) {
-            rollbackTransaction(transaction);
-            throw new DatabaseOperationException();
-        }
-    }
-
-    public void deleteByUser(User user) {
-        EntityTransaction transaction = null;
-        try (EntityManager entityManager = PersistenceUtil.getInstance().createEntityManager()) {
-            transaction = entityManager.getTransaction();
-            transaction.begin();
-
-            entityManager
-                    .createQuery("delete from Session s where user=:user")
-                    .setParameter("user", user)
-                    .executeUpdate();
-
-            transaction.commit();
         } catch (PersistenceException exception) {
             rollbackTransaction(transaction);
             throw new DatabaseOperationException();
