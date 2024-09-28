2d520c0ed9913d0c8f789e08fbf426109ab2832d
damvih <damvih03.official@gmail.com>
2024-09-28 13:53:43 +1000

    feat: add UnixLocalDateTimeDeserializer

---
+++ src/main/java/com/damvih/utils/UnixLocalDateTimeDeserializer.java
@@ -0,0 +1,23 @@
+package com.damvih.utils;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class UnixLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        long unixTime = jsonParser.getLongValue();
+        return LocalDateTime.ofInstant(
+                Instant.ofEpochSecond(unixTime), ZoneId.systemDefault()
+        );
+    }
+
+}
