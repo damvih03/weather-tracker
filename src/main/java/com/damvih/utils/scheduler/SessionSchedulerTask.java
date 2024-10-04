6f09339d0915a27fc1cbb20ea7fc1eba3eaead79
damvih <damvih03.official@gmail.com>
2024-10-04 21:23:22 +1000

    feat: add RequiredArgsConstructor annotation

--- src/main/java/com/damvih/utils/scheduler/SessionSchedulerTask.java
+++ src/main/java/com/damvih/utils/scheduler/SessionSchedulerTask.java
@@ -1,12 +1,14 @@
 package com.damvih.utils.scheduler;
 
 import com.damvih.dao.SessionDao;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@RequiredArgsConstructor
 public class SessionSchedulerTask implements Runnable {
 
-    private SessionDao sessionDao;
+    private final SessionDao sessionDao;
 
     @Override
     public void run() {
