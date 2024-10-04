a16f9c6e2b189a1231a19824c18cdc2e0481a0c1
damvih <damvih03.official@gmail.com>
2024-10-04 22:01:51 +1000

    feat: add SessionDaoTest

---
+++ src/test/java/com/damvih/dao/SessionDaoTest.java
@@ -0,0 +1,64 @@
+package com.damvih.dao;
+
+import com.damvih.entities.Session;
+import com.damvih.entities.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+public class SessionDaoTest {
+
+    private static SessionDao sessionDao;
+    private static User user;
+
+    @BeforeAll
+    public static void setup() {
+        sessionDao = new SessionDao();
+
+        UserDao userDao = new UserDao();
+
+        user = userDao.save(
+                User.builder()
+                        .login("userSession")
+                        .password("password")
+                        .build()
+        );
+    }
+
+    @Test
+    public void testDeleteActualSession() {
+        UUID sessionId = UUID.randomUUID();
+
+        sessionDao.save(new Session(
+                sessionId,
+                user,
+                LocalDateTime.now().plusHours(6)
+        ));
+
+        sessionDao.deleteIfExpired(LocalDateTime.now());
+
+        Optional<Session> session = sessionDao.findById(sessionId);
+        Assertions.assertTrue(session.isPresent());
+    }
+
+    @Test
+    public void testDeleteExpiredSession() {
+        UUID sessionId = UUID.randomUUID();
+
+        sessionDao.save(new Session(
+                sessionId,
+                user,
+                LocalDateTime.now().minusHours(6 + 1)
+        ));
+
+        sessionDao.deleteIfExpired(LocalDateTime.now());
+
+        Optional<Session> session = sessionDao.findById(sessionId);
+        Assertions.assertTrue(session.isEmpty());
+    }
+
+}
