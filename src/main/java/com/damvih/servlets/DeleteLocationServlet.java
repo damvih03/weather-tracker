5d9171f94243340f42908d150b40721022e74738
damvih <damvih03.official@gmail.com>
2024-10-12 13:10:49 +1000

    fix: extract delete location method from LocationsServlet to DeleteLocationServlet

--- src/main/java/com/damvih/servlets/DeleteLocationServlet.java
+++ src/main/java/com/damvih/servlets/DeleteLocationServlet.java
@@ -1,36 +1,27 @@
 package com.damvih.servlets;
 
 import com.damvih.dto.LocationRequestDto;
-import com.damvih.dto.api.geocoding.GeocodingApiResponseDto;
 import com.damvih.entities.Location;
 import com.damvih.entities.Session;
 import com.damvih.exceptions.LocationNotFoundException;
 import com.damvih.services.LocationService;
 import com.damvih.services.UserLocationService;
-import com.damvih.services.WeatherApiService;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 
-@WebServlet("/locations")
-public class LocationsServlet extends BaseServlet {
+@WebServlet("/delete-location")
+public class DeleteLocationServlet extends BaseServlet {
 
-    private WeatherApiService weatherApiService;
     private LocationService locationService;
     private UserLocationService userLocationService;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        weatherApiService = (WeatherApiService) config
-                .getServletContext()
-                .getAttribute("WeatherApiService");
-
         locationService = (LocationService) config
                 .getServletContext()
                 .getAttribute("LocationService");
@@ -43,31 +34,22 @@ public class LocationsServlet extends BaseServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String cityName = request.getParameter("locationName");
-
-        GeocodingApiResponseDto geocodingApiResponse = weatherApiService.getLocationsByName(cityName);
-
-        webContext.setVariable("geocodingApiResponse", geocodingApiResponse);
-        templateEngine.process("locations", webContext, response.getWriter());
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Session session = (Session) request.getAttribute("session");
 
-        String cityName = request.getParameter("locationName");
         String longitude = request.getParameter("lon");
         String latitude = request.getParameter("lat");
-        
+
         LocationRequestDto locationRequestDto = LocationRequestDto.builder()
-                .name(cityName)
                 .longitude(getValueInBigDecimal(longitude))
                 .latitude(getValueInBigDecimal(latitude))
                 .build();
 
-        Location location = locationService.findOrSave(locationRequestDto);
-        userLocationService.save(session.getUser(), location);
+        Location location = locationService
+                .findByCoordinates(locationRequestDto)
+                .orElseThrow(LocationNotFoundException::new);
+
+        userLocationService.delete(session.getUser(), location);
         response.sendRedirect("/home");
     }
 
