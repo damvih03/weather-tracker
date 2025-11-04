d99fda651b79b7dce09d06a2bb91a7e253fe2993
damvih <damvih.official@gmail.com>
2025-11-04 10:22:30 +1000

    feat: add HibernateConfig with properties files (datasource.properties, hibernate.properties)

---
+++ src/main/java/com/damvih/HibernateConfig.java
@@ -0,0 +1,40 @@
+package com.damvih;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.hibernate.HibernateTransactionManager;
+import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+    public static final String ENTITIES_PACKAGE = "com.damvih.entities";
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig("datasource.properties");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan(ENTITIES_PACKAGE);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(LocalSessionFactoryBean sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory.getObject());
+        return transactionManager;
+    }
+
+}
