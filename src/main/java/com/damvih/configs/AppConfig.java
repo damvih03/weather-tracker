7d96964df2fa1467fa0b2db22adecc1568a32948
damvih <damvih.official@gmail.com>
2025-11-06 00:53:22 +1000

    feat: add ModelMapper to Spring context

--- src/main/java/com/damvih/configs/AppConfig.java
+++ src/main/java/com/damvih/configs/AppConfig.java
@@ -1,10 +1,17 @@
 package com.damvih.configs;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.damvih.dao", "com.damvih.services"})
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
