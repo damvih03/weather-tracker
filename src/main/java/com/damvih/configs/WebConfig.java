4294110421b6508d8d4bdcc98c488729f09e5211
damvih <damvih.official@gmail.com>
2025-11-05 12:30:20 +1000

    feat: add resource handlers in WebConfig by implementing WebMvcConfigurer interface

--- src/main/java/com/damvih/configs/WebConfig.java
+++ src/main/java/com/damvih/configs/WebConfig.java
@@ -3,10 +3,19 @@ package com.damvih.configs;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.damvih.controllers"})
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
 
 }
