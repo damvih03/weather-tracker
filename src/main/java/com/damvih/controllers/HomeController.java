c999a02cfe09f2078e99c228598c31cc873070e0
damvih <damvih.official@gmail.com>
2025-11-12 15:19:30 +1000

    feat: add session interceptor for controllers

--- src/main/java/com/damvih/controllers/HomeController.java
+++ src/main/java/com/damvih/controllers/HomeController.java
@@ -1,24 +1,19 @@
 package com.damvih.controllers;
 
 import com.damvih.dto.SessionDto;
-import com.damvih.services.SessionService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final SessionService sessionService;
-
     @GetMapping("/")
-    public String getHomePage(@CookieValue(name = "SESSION_ID") String sessionId, Model model) {
-        SessionDto sessionDto = sessionService.getValid(UUID.fromString(sessionId));
+    public String getHomePage(HttpServletRequest request, Model model) {
+        SessionDto sessionDto = (SessionDto) request.getAttribute("session");
         model.addAttribute("username", sessionDto.getUserDto().getUsername());
         return "index";
     }
