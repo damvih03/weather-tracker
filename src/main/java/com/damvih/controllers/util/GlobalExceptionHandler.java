76d6b96e6e0b6332c7a70767266d54785f439033
damvih <damvih.official@gmail.com>
2025-11-12 18:44:46 +1000

    feat: redirect all no-auth requests to sign-in page

--- src/main/java/com/damvih/controllers/util/GlobalExceptionHandler.java
+++ src/main/java/com/damvih/controllers/util/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.damvih.controllers.util;
 
+import com.damvih.exceptions.InvalidSessionException;
 import com.damvih.exceptions.InvalidUserDataException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidSessionException.class)
+    public String handleInvalidSessionException() {
+        return "redirect:/sign-in";
+    }
 
     @ExceptionHandler(InvalidUserDataException.class)
     public String handleInvalidUserDataException(Model model) {
