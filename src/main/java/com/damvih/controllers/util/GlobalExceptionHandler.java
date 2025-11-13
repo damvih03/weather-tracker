53bfebf0cc0335b5280709c93552e1ce93651397
damvih <damvih.official@gmail.com>
2025-11-13 14:34:17 +1000

    feat: add alert to sign-up page when username already exists

--- src/main/java/com/damvih/controllers/util/GlobalExceptionHandler.java
+++ src/main/java/com/damvih/controllers/util/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.damvih.controllers.util;
 
 import com.damvih.exceptions.InvalidSessionException;
 import com.damvih.exceptions.InvalidUserDataException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,14 @@ public class GlobalExceptionHandler {
     public String handleInvalidUserDataException(Model model) {
         model.addAttribute("error", "Invalid username or password.");
         return "sign-in";
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public String handleDataIntegrityViolationException(DataIntegrityViolationException exception, Model model) {
+        if (exception.getMessage().contains("users_username_key")) {
+            model.addAttribute("error", "Username is already taken. Try another one.");
+        }
+        return "sign-up";
     }
 
 }
