37e14c277f62e117aff4ef412cf7919c65c11482
damvih <damvih.official@gmail.com>
2025-11-17 22:58:30 +1000

    feat: test AuthenticationService, SessionService

---
+++ src/test/java/com/damvih/services/AuthenticationServiceTest.java
@@ -0,0 +1,66 @@
+package com.damvih.services;
+
+import com.damvih.configs.AppConfig;
+import com.damvih.configs.DataSourceConfig;
+import com.damvih.configs.FlywayConfig;
+import com.damvih.configs.HibernateConfig;
+import com.damvih.dto.SessionDto;
+import com.damvih.dto.UserRegistrationDto;
+import com.damvih.dto.UserRequestDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppConfig.class, DataSourceConfig.class, FlywayConfig.class, HibernateConfig.class})
+public class AuthenticationServiceTest {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Test
+    public void testCreatingUserWithExistingUsername() {
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto(
+                "user1",
+                "password1",
+                "password1"
+        );
+        authenticationService.signUp(userRegistrationDto);
+
+        DataIntegrityViolationException exception = Assertions.assertThrows(
+                DataIntegrityViolationException.class,
+                () -> authenticationService.signUp(userRegistrationDto)
+        );
+        Assertions.assertTrue(exception.getMessage().contains("users_username_key"));
+    }
+
+    @Test
+    public void testCorrectCreatingUser() {
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto(
+                "user2",
+                "password2",
+                "password2"
+        );
+
+        SessionDto sessionDto = authenticationService.signUp(userRegistrationDto);
+
+        Assertions.assertDoesNotThrow(() -> userService.get(
+                new UserRequestDto(
+                        userRegistrationDto.getUsername(),
+                        userRegistrationDto.getPassword()
+                ))
+        );
+        Assertions.assertDoesNotThrow(() -> sessionService.getValid(sessionDto.getId()));
+    }
+
+}
