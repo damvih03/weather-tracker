f61a92faf1cb7f472da38772e092e8740a4ad23d
damvih <damvih.official@gmail.com>
2025-11-17 22:57:55 +1000

    feat: define flyway config in test env

---
+++ src/test/java/com/damvih/configs/FlywayConfig.java
@@ -0,0 +1,27 @@
+package com.damvih.configs;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public Flyway flyway(DataSource dataSource, @Value("${schema}") String schemaName) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .defaultSchema(schemaName)
+                .cleanDisabled(false)
+                .locations("classpath:/db/test-migration")
+                .load();
+
+        flyway.clean();
+        flyway.migrate();
+        return flyway;
+    }
+
+}
