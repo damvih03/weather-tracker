6b4d365c0f729f505beb72e0e2450991b01c2103
damvih <damvih.official@gmail.com>
2025-11-17 14:33:08 +1000

    fix: add session mapper to ModelMapper

--- src/main/java/com/damvih/configs/AppConfig.java
+++ src/main/java/com/damvih/configs/AppConfig.java
@@ -1,5 +1,7 @@
 package com.damvih.configs;
 
+import com.damvih.dto.SessionDto;
+import com.damvih.entities.Session;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +13,13 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.typeMap(Session.class, SessionDto.class).addMappings(mapper -> {
+            mapper.map(Session::getUser, SessionDto::setUserDto);
+        });
+
+        return modelMapper;
     }
 
 }
