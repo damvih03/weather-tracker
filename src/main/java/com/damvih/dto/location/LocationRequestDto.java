3f9fd8bc203360071520f1cca1424ed3a48f274d
damvih <damvih.official@gmail.com>
2025-11-22 22:02:28 +1000

    feat: validate LocationRequestDto

--- src/main/java/com/damvih/dto/location/LocationRequestDto.java
+++ src/main/java/com/damvih/dto/location/LocationRequestDto.java
@@ -1,5 +1,8 @@
 package com.damvih.dto.location;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,17 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class LocationRequestDto {
 
+    @NotNull
+    @DecimalMin(value = "-90.0")
+    @DecimalMax(value = "90.0")
     private BigDecimal latitude;
+
+    @NotNull
+    @DecimalMin(value = "-180.0")
+    @DecimalMax(value = "180.0")
     private BigDecimal longitude;
+
+    @NotNull
     private String name;
 
 }
